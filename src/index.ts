ab392beb7746d143f75b19598a430b9264468c59
JiangWeixian <jiangweixian1994@gmail.com>
2022-08-20 16:32:12 +0800

    :construction:  filter routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,41 @@
-import { Table } from '@/md'
+import { matchPath } from 'react-router'
+import type { RouteObject } from 'react-router'
 
-export const display = (): ReturnType<typeof Table> => {
-  return Table()
+type ExtendRouteObject = RouteObject & {
+  absolutePath: string
 }
 
-export const welcome = () => {
-  console.log('hello world')
+const joinPaths = (paths: string[]): string => {
+  let pathname = paths.join('/').replace(/\/\/+/g, '/')
+  if (!pathname.startsWith('/')) {
+    pathname = `/${pathname}`
+  }
+  return pathname
+}
+
+export const filterRoutes = (routes: RouteObject[], pathname: string, parentPath = '') => {
+  const matchedRoutes: RouteObject[] = []
+  routes.forEach((route) => {
+    const next: ExtendRouteObject = Object.assign({}, route) as ExtendRouteObject
+    const absolutePath = joinPaths([parentPath || '/', next.path || ''])
+    let matched = false
+    if (next.path) {
+      next.absolutePath = absolutePath
+      matched = !!matchPath(
+        {
+          path: next.absolutePath,
+          caseSensitive: next.caseSensitive,
+          end: !next.children,
+        },
+        pathname,
+      )
+    }
+    if (route.children) {
+      next.children = filterRoutes(route.children, pathname, absolutePath)
+    }
+    if (matched || next.children) {
+      matchedRoutes.push(next)
+    }
+  })
+  return matchedRoutes
 }
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,5 +1,49 @@
-import { welcome } from '../src'
+import type { RouteObject } from 'react-router'
 import { describe, test, expect, vi } from 'vitest'
+
+let userEditRoute: RouteObject = {
+  path: "edit",
+};
+let userProfileRoute: RouteObject = {
+  path: ":id",
+  children: [userEditRoute],
+};
+let usersRoute: RouteObject = {
+  path: "/users",
+  children: [{ index: true }, userProfileRoute],
+};
+let indexWithPathRoute: RouteObject = {
+  path: "/withpath",
+  index: true,
+};
+let layoutRouteIndex: RouteObject = {
+  path: "/layout",
+  index: true,
+};
+let layoutRoute: RouteObject = {
+  path: "/layout",
+  children: [
+    { path: "item", },
+    { path: ":id", },
+    { path: "*", },
+  ],
+};
+
+let routes = [
+  { path: "/", element },
+  {
+    path: "/home",
+    children: [
+      { index: true, },
+      { path: "*" },
+    ],
+  },
+  indexWithPathRoute,
+  layoutRoute,
+  layoutRouteIndex,
+  usersRoute,
+  { path: "*" },
+];
 
 describe('index', () => {
   test('demo part', () => {
